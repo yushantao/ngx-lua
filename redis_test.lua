152d9bef378b9d8318c035dd317788fabd6118bc
aligatao <yushantao@daling.com>
2015-12-01 11:53:03 +0800

    repair logic

--- redis_test.lua
+++ redis_test.lua
@@ -6,20 +6,14 @@ local args = ngx.req.get_headers()["Host"]
 ---取redis内容
 local red = redis.new()
 red:set_timeout(60000)
-local ok, err = red:connect("127.0.0.1", 6379)
+local ok, err = red:connect("10.36.4.86", 6379)
 if not ok then
     ngx.say("failed to connect: ", err)
     return
 end
 local ok, err = red:get(args)
-if not ok then
-        ngx.say("failed to get key: ", err)
-        return
-end
 if ok == ngx.null then
-        ngx.say("key not found.")
         return
-end
-----	ngx.header["X-My-Header"] = ok;
---- set header to proxy backend
+else
  ngx.req.set_header("X-Fooooooooooooo", ok)
+end
